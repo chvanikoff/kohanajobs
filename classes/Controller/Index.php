d1b684044aaaddd5f3afe516a7cd5b1418ef4ce2
Chvanikoff <chvanikoff@gmail.com>
2012-08-05 23:12:09 +0400

    Removed unnecessary code
    Jobs list on Index page was replaced with HMVC-request previously

--- classes/Controller/Index.php
+++ classes/Controller/Index.php
@@ -1,8 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Index extends Controller_Front {
-	public function GET_index()
-	{
-		$this->view->jobs = ORM::factory('job')->find_all();
-	}
+	
 } // End Welcome
